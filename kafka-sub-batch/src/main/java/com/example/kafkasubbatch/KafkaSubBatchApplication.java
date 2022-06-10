cdde14296d7c0e453527fcd383ee787ae93f3190
kgsong <kyunggeun.song@seavantage.com>
2022-06-10 15:18:41 +0900

    kafka 와 spring batch 통합을 위한 초기 설정

---
+++ kafka-sub-batch/src/main/java/com/example/kafkasubbatch/KafkaSubBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkasubbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSubBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSubBatchApplication.class, args);
+    }
+
+}
---
+++ kafka-sub-batch/src/test/java/com/example/kafkasubbatch/KafkaSubBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkasubbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSubBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
