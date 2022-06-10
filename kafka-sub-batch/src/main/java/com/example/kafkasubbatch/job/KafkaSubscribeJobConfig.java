de5be699ae404b1f8ce199b0105d43d30989c05a
kgsong <kyunggeun.song@seavantage.com>
2022-06-10 15:47:47 +0900

    KafkaItemReader 구현

---
+++ kafka-sub-batch/src/main/java/com/example/kafkasubbatch/job/KafkaSubscribeJobConfig.java
@@ -0,0 +1,57 @@
+package com.example.kafkasubbatch.job;
+
+import java.util.Properties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.kafka.KafkaItemReader;
+import org.springframework.batch.item.kafka.builder.KafkaItemReaderBuilder;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class KafkaSubscribeJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    Job kafkaJob() {
+        return jobBuilderFactory.get("kafkaJob")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(5)
+                .reader(kafkaItemReader())
+                .writer(items -> log.info("items: {}", items))
+                .build();
+    }
+
+    @Bean
+    KafkaItemReader<String, String> kafkaItemReader() {
+        Properties properties = new Properties();
+        properties.putAll(kafkaProperties.buildConsumerProperties());
+
+        return new KafkaItemReaderBuilder<String, String>()
+                .name("kafkaItemReader")
+                .topic("test")
+                .partitions(0)
+                .consumerProperties(properties)
+                .build();
+    }
+
+}
