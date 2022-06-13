97a80b0bc6ab4547c3c96f77f43126212b946be0
kgsong <kyunggeun.song@seavantage.com>
2022-06-13 10:30:05 +0900

    refactor: optimize import

--- kafka-sub-batch/src/main/java/com/example/kafkasubbatch/job/KafkaSubscribeJobConfig.java
+++ kafka-sub-batch/src/main/java/com/example/kafkasubbatch/job/KafkaSubscribeJobConfig.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.kafka.KafkaItemReader;
